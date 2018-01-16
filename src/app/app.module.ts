a1f802827ff166e460a648b88719d4ada3c788c5
Austin <joha0033@gmail.com>
2018-01-15 20:42:19 -0700

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,11 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     AlertModule.forRoot(),
