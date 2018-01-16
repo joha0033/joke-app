7e8562ce548a932e364fa6a7441373a3840a7627
Austin <joha0033@gmail.com>
2018-01-15 21:19:35 -0700

    deployment build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './shared/header/header.component'
+import { FooterComponent } from './shared/footer/footer.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
